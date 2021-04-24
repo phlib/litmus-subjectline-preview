c6e9d3516adbe6ff837d1e49e2619f841a087aa3
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2021-04-24 06:02:28 +0100

    Add specific tests for SubjectPreview methods

--- tests/SubjectPreviewTest.php
+++ tests/SubjectPreviewTest.php
@@ -12,33 +12,98 @@ use PHPUnit\Framework\TestCase;
  */
 class SubjectPreviewTest extends TestCase
 {
-    protected $subjectPreview;
-
-    public function setUp(): void
+    /**
+     * @dataProvider dataGetSubject
+     */
+    public function testGetSubject(string $value, string $expected): void
     {
-        $this->subjectPreview = new SubjectPreview();
-        $this->subjectPreview
-            ->setSubject(
-                'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz' .
-                'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
-            )
-            ->setBody(
-                'zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba' .
-                'zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba'
-            )
-            ->setSender('aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz')
-        ;
+        $subjectPreview = new SubjectPreview();
+
+        $subjectPreview->setSubject($value);
+
+        static::assertSame($expected, $subjectPreview->getSubject());
     }
 
-    protected function tearDown(): void
+    public function dataGetSubject(): array
     {
-        $this->subjectPreview = null;
+        $baseString = sha1(uniqid()); // 40 chars
+        return [
+            'shortSubject' => [
+                str_repeat($baseString, 2),
+                str_repeat($baseString, 2),
+            ],
+            'longSubject' => [
+                str_repeat($baseString, 5),
+                substr(str_repeat($baseString, 3), 0, 100), // Truncate to 100 chars
+            ],
+        ];
     }
 
-    public function testSubjectPreview(): void
+    /**
+     * @dataProvider dataGetBody
+     */
+    public function testGetBody(string $value, string $expected): void
     {
-        static::assertEquals(100, strlen($this->subjectPreview->getSubject()));
-        static::assertEquals(100, strlen($this->subjectPreview->getBody()));
-        static::assertEquals(50, strlen($this->subjectPreview->getSender()));
+        $subjectPreview = new SubjectPreview();
+
+        $subjectPreview->setBody($value);
+
+        static::assertSame($expected, $subjectPreview->getBody());
+    }
+
+    public function dataGetBody(): array
+    {
+        $baseString = sha1(uniqid()); // 40 chars
+        return [
+            'shortBody' => [
+                str_repeat($baseString, 2),
+                str_repeat($baseString, 2),
+            ],
+            'longBody' => [
+                str_repeat($baseString, 5),
+                substr(str_repeat($baseString, 3), 0, 100), // Truncate to 100 chars
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataGetSender
+     */
+    public function testGetSender(string $value, string $expected): void
+    {
+        $subjectPreview = new SubjectPreview();
+
+        $subjectPreview->setSender($value);
+
+        static::assertSame($expected, $subjectPreview->getSender());
+    }
+
+    public function dataGetSender(): array
+    {
+        $baseString = sha1(uniqid()); // 40 chars
+        return [
+            'shortSender' => [
+                $baseString,
+                $baseString,
+            ],
+            'longSender' => [
+                str_repeat($baseString, 5),
+                substr(str_repeat($baseString, 3), 0, 50), // Truncate to 50 chars
+            ],
+        ];
+    }
+
+    public function testGetEmailClient(): void
+    {
+        $checkValue = sha1(uniqid());
+
+        $subjectPreview = new SubjectPreview();
+        $subjectPreview->setSubject($checkValue);
+
+        $emailClient = $subjectPreview->getEmailClient('ol2003');
+
+        self::assertEquals('ol2003', $emailClient->getSlug());
+
+        self::assertStringContainsString($checkValue, $emailClient->getUrl(false));
     }
 }
